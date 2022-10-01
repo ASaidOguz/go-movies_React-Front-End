6d3031cf72faf63d0efe90be9b597add66bf7679
ASaidOguz <ahmetsaid2136@gmail.com>
2022-10-01 14:42:14 +0300

    OneMovie component is created a d initialized

--- src/component/OneMovie.js
+++ src/component/OneMovie.js
@@ -1,32 +1,77 @@
 import React, { Component, Fragment } from 'react'
 
 export default class OneMovie extends Component{
-state={movie:{}}
+state={movie:{},isLoaded:false,error:null}
 componentDidMount(){
-    this.setState(
-        {movie:{
-            id:this.props.match.params.id,
-            title:"Some movie",
-            runtime:150,
+    fetch("http://localhost:4000/v1/movie/"+this.props.match.params.id)
+      .then((response) => {
+        console.log("Status code is", response.status);
+        if (response.status !== "200") {
+          let err = Error;
+          err.message = "Invalid response code: " + response.status;
+          this.setState({error: err});
         }
-
+        return response.json();
+      })
+      .then((json) => {
+        this.setState({
+          movie: json.movie,
+          isLoaded: true,
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
         }
-    )
+        );
+      });
 }
 render(){
+
+     const{movie,isLoaded,error}=this.state
+    if(movie.genres){
+       movie.genres=Object.values(movie.genres)
+    }else{
+        movie.genres=[];
+    }
+    
+    
+     if(error){
+        return <div>Error: {error.message}</div>
+      }
+      else if (!isLoaded) {
+        return <p>Loading...</p>;
+      } else {
     return(
         <Fragment>
-        <h2>Movie: {this.state.movie.title}   {this.state.movie.id}</h2>
+        <h2>Movie: {movie.title}   ({movie.year})</h2>
+        <div className='float-start'>
+            <small>Rating : {movie.mpaa_rating}</small>
+        </div>
+        <div className='float-end'>
+            {movie.genres.map((m,index)=>( 
+                <span className='badge bg-secondary me-1' key={index}>
+                   {m}
+                </span>
+            ))}
+        </div>
+        <div className='clearfix'></div>
+        <hr/>
         <table className='table table-compact table-striped'>
             <thead></thead>
             <tbody>
                 <tr>
                     <td><strong>Title</strong></td>
-                    <td><strong>{this.state.movie.title}</strong></td>
+                    <td><strong>{movie.title}</strong></td>
+                </tr>
+                <tr>
+                    <td><strong>Description:</strong></td>
+                    <td>{movie.description} </td>
                 </tr>
                 <tr>
                     <td><strong>Runtime</strong></td>
-                    <td><strong>{this.state.movie.runtime} minutes</strong></td>
+                    <td><strong>{movie.runtime} minutes</strong></td>
                 </tr>
             </tbody>
         </table>
@@ -34,5 +79,5 @@ render(){
 
         </Fragment>
     )
-}
+}}
 }
