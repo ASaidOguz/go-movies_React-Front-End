3f93f4d94381e5748799fead5556a091782415b4
ASaidOguz <ahmetsaid2136@gmail.com>
2022-10-04 13:59:49 +0300

    InputArea component added for form control

---
+++ src/component/form-components/InputArea.js
@@ -0,0 +1,18 @@
+const InputArea=(props)=>{
+
+return(
+            <div className='mb-3'>
+               <label htmlFor={props.description} className='form-label'>
+                {props.title}
+               </label>
+               <textarea className='form-control'
+                         id={props.name}
+                         name={props.name} 
+                         rows={props.rows}
+                        onChange={props.handleChange}
+                        value={props.value}
+                        /> 
+            </div>
+)
+}
+export default InputArea
