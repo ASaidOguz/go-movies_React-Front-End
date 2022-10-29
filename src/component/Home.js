34960bba2ad2588177a71c10136f4a1cfff4b6ac
ASaidOguz <ahmetsaid2136@gmail.com>
2022-10-29 14:51:06 +0300

    added image on home page

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,5 +1,6 @@
 import React,{Component} from'react'
-
+import cine_gop from "./../images/Go_gopher_five_years.jpg"
+import "./../images/image.css"
 export default class Home extends Component{
 
 
@@ -7,8 +8,12 @@ export default class Home extends Component{
 render(){
 
     return(
-       
+       <div className='text-center'>
         <h1>This is the home Page</h1>
+        <hr/>
+        <img src={cine_gop} alt='gophers cinematography'/>
+        </div> 
+        
         
     );
 }
